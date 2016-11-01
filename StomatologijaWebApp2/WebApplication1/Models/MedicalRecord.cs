0dc61810ecee88ac98347466286f4dbf490223b8
Jusuf Koric <jusufk12@hotmail.com>
2016-11-02 00:46:23 +0100

    Medical Record basic model

--- StomatologijaWebApp2/WebApplication1/Models/MedicalRecord.cs
+++ StomatologijaWebApp2/WebApplication1/Models/MedicalRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace WebApplication1.Models
 {
     public class MedicalRecord
     {
+        [Required]
+        public  int Id { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public double Bill { get; set; }//dont know what annotation could add, maybe none
+
+        //gledao sam ovo kako si ti radio ovo u EF al spominju se nakvi kljucevi pa nisam htio nista dodavati da ne zabrljam sta
     }
 }
