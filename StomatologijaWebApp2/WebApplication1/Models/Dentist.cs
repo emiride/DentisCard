02955e75cd2d2fd2ca39cab58c39f7eafa65c605
Omer <Frenky_93@hotmail.com>
2016-11-01 19:41:05 +0100

    2november

--- StomatologijaWebApp2/WebApplication1/Models/Dentist.cs
+++ StomatologijaWebApp2/WebApplication1/Models/Dentist.cs
@@ -1,27 +1,20 @@
-﻿namespace WebApplication1.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
 {
     public class Dentist
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public  MyProperty { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public EmploymentStatus? EmploymentStatus { get; set; } //This value is nullable
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
 
 
-
-
-
-
-
-
-Id
-First Name
-Last Name
-Password
-Date Of Birth
-Employment Status
-Phone Number
-Address
-Date Created
