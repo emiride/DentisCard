9b0f271f0328057d77d3fc2e20e9652d89731b67
Emir Hodžić <emir.hodzic@outlook.com>
2016-11-01 16:33:31 +0100

    Patient model expanded with some new annotations and ApplicationDbContext class moved to the seperate file (there is a convention that each class should be in seperate file). Need to mention that ApplicationDbContext was in IdentityModels

---
+++ StomatologijaWebApp2/WebApplication1/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- StomatologijaWebApp2/WebApplication1/Models/Patient.cs
+++ StomatologijaWebApp2/WebApplication1/Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
@@ -14,18 +15,36 @@ namespace WebApplication1.Models
     public class Patient
     {
         //The following are the properties of the Patient class
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] //DataType is very powerfull Data Annotation, which can affect our view if we use EF, so I will try to accomplish as much as possible with that
         public string Password { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
+
         public EmploymentStatus? EmploymentStatus { get; set; } //Question mark means that this value is nullable
+
+        [Required]
         public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
         public string Address { get; set; }
 
 
         //The following are the relations to other classes (One patient can have one Dentist and many MedicalRecords)
         public virtual Dentist Dentist { get; set; }
+
         public virtual ICollection<MedicalRecord> MedicalRecords { get; set; }
 
     }
