a2ef637ca3fd61ff9e23926035a030c1b1c44eef
Omer <Frenky_93@hotmail.com>
2016-11-01 19:25:10 +0100

    1st november

--- StomatologijaWebApp2/WebApplication1/Models/Dentist.cs
+++ StomatologijaWebApp2/WebApplication1/Models/Dentist.cs
@@ -2,6 +2,26 @@
 {
     public class Dentist
     {
-
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public  MyProperty { get; set; }
     }
 }
+
+
+
+
+
+
+
+
+Id
+First Name
+Last Name
+Password
+Date Of Birth
+Employment Status
+Phone Number
+Address
+Date Created
