6225953f5d1ba8cec918bb6c1f12f8f0eca1f3b3
Emir Hodžić <emir.hodzic@outlook.com>
2016-11-07 11:30:11 +0100

    LandingPage added with all the content

--- StomatologijaWebApp2/WebApplication1/Controllers/HomeController.cs
+++ StomatologijaWebApp2/WebApplication1/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
 {
@@ -24,6 +20,11 @@ namespace WebApplication1.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult LandingPage()
+        {
             return View();
         }
     }
