bd887e525efe681554484dc0aa7d2a8879ff8139
Jusuf Koric <jusufk12@hotmail.com>
2016-11-10 02:17:10 +0100

    Teeth Positions done and half of ToothState

--- StomatologijaWebApp2/WebApplication1/Enumerations/ToothPosition.cs
+++ StomatologijaWebApp2/WebApplication1/Enumerations/ToothPosition.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Enumerations
     //TODO Jusuf
     public enum ToothPosition
     {
+        //GornjiDesno
         gd1 = 11,
         gd2 = 12,
         gd3 = 13,
@@ -17,6 +18,7 @@ namespace WebApplication1.Enumerations
         gd7 = 17,
         gd8 = 18,
 
+        //GodnjiLijevo
         gl1 = 21,
         gl2 = 22,
         gl3 = 23,
@@ -25,5 +27,26 @@ namespace WebApplication1.Enumerations
         gl6 = 26,
         gl7 = 27,
         gl8 = 28,
+
+        //DonjiDesno
+        dd1 = 41,
+        dd2 = 42,
+        dd3 = 43,
+        dd4 = 44,
+        dd5 = 45,
+        dd6 = 46,
+        dd7 = 47,
+        dd8 = 48,
+
+        //DonjiLijevo
+        dl1 = 31,
+        dl2 = 32,
+        dl3 = 33,
+        dl4 = 34,
+        dl5 = 35,
+        dl6 = 36,
+        dl7 = 37,
+        dl8 = 38
+
     }
 }
--- StomatologijaWebApp2/WebApplication1/Enumerations/ToothState.cs
+++ StomatologijaWebApp2/WebApplication1/Enumerations/ToothState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,30 @@ namespace WebApplication1.Enumerations
     public enum ToothState
     {
     //TODO Jusuf
-        Healthy = 1
+    
+    //In order to make fully readable we cannot use just like before these enums beacuse state of tooth is small 'description' with few words
+    //so we can use toDescription method and first part goes like this below   
+        Healthy = 1,
+        [Description("Caries and one surface filling ")]
+        Caries1 = 2,
+        [Description("Two surface fillings")]
+        Caries2 = 3,
+        [Description("Three and more fillings")]
+        Caries3 = 4,
+        [Description("Curing")] //I left this one as Descriptios because we may add something 
+        curing = 5,
+        [Description("One canal curing")]
+        CanalCuring1 = 6,
+        [Description("Two canals curings")]
+        CanalCuring2 = 7,
+        [Description("Three canals curings (possible extraction)")]
+        CanalCuring3 = 8,
+        [Description("No tooth")]
+        NoTooth = 9,
+        //ne sjecam se sta je deseti trebao biti
+
+
+
+
     }
 }
