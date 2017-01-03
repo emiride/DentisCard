c787beceac5be37554f2408500c6c61240408142
Omer <Frenky_93@hotmail.com>
2017-01-03 18:58:22 +0100

    Dentis is able to see his patients on the Home page

---
+++ WebApplication1/WebApplication1/ViewModels/DentistsListOfPatient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class DentistsListOfPatient
+    {
+        public IEnumerable<Models.Dentist> Dentists { get; set; }
+        public IEnumerable<Patient> Patients { get; set; }
+    }
+
+}
